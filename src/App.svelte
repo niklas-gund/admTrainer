3529e87a7fde02a7707cef307fd018b02adbf9ef
ng28 <niklas.gund@uni-ulm.de>
2022-02-19 11:01:26 +0100

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,80 @@
+<script lang="ts">
+	import RSC from './components/ReedSalomonTrainer.svelte'
+
+	const moduleNames: string[] = ["RSC", "test1", "test2", "test3"]
+	var selectedModule: string = "RSC"
+</script>
+
+<main class="container mx-auto">
+	<h1>ADM - Trainer</h1>
+	<div class="flex">
+		{#each moduleNames as module, index}
+		<div class="flex-initial w-32 moduleSelectorItem {selectedModule == module?"moduleSelectorSelected":""}"
+		on:click="{() => selectedModule = module}">
+			{module}
+		</div>
+		{/each}
+	</div>
+	<div class="spacer"/>
+	{#if selectedModule == "RSC"}
+	<RSC/>
+	{/if}
+</main>
+
+<style lang="postcss" global>
+	@tailwind base;
+	@tailwind components;
+	@tailwind utilities;
+
+	:root{
+		--lightBlue: #8ecae6;
+		--blue: #219ebc;
+		--darkBlue: #023047;
+		--yellow: #ffb703;
+		--orange: #fb8500;
+	}
+
+	h1{
+		color: var(--darkBlue);
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	h2{
+		color: var(--darkBlue);
+		text-transform: uppercase;
+		font-size: 2em;
+		font-weight: 500;
+	}
+	h3{
+		color: var(--darkBlue);
+		text-transform: uppercase;
+		font-size: 2em;
+		font-weight: 300;
+	}
+
+	.spacer{
+		height: 10px;
+	}
+
+	* {
+		font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif
+	}
+
+	.moduleSelectorItem{
+		background-color: var(--blue);
+		margin: 0 10px 0 10px;
+		padding: 5px 20px;
+		border-radius: 5px;
+		font-weight: 500;
+	}
+
+	.moduleSelectorSelected{
+		background-color: var(--darkBlue);
+		color: white;
+	}
+
+  </style>
+
+  <!-- colors: 8ecae6 219ebc 023047 ffb703 fb8500-->
---
+++ src/components/ReedSalomonTrainer.svelte
@@ -0,0 +1,62 @@
+<script lang="ts">
+    const primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
+    var countLambdas = 4;
+    $: restKlassenRingOrdnung = rkrExponent**primeBasis;
+    var primeBasis = 1;
+    var rkrExponent = 7;
+    $: lambdas = generateLambdas(restKlassenRingOrdnung, countLambdas)
+
+
+function generateLambdas(restKlassenRingOrdnung: number,countLambdas: number): number[] {
+    var toReturn: number[] = []
+    for (var i = 0; i < countLambdas; i++){
+        toReturn.push(i+1)
+    }
+    return toReturn;
+}
+</script>
+
+<div>
+
+
+    <h2>Reed Solomon Codes</h2>
+    <h3>Parameter auswahl</h3>
+
+    <div class="flex flex-row space-x-4">
+        <div>Anzahl Lambdas</div>
+        <div>
+            <input type="number" min="2" max="20" bind:value={countLambdas}>
+        </div>
+        <div></div>
+        {#each lambdas as lambda,i}
+        <div>λ<sub>{i+1}</sub> =</div>
+        <input class="w-12" type="number" bind:value={lambdas[i]}>
+        {/each}
+    </div>
+
+    <div class="flex flex-row space-x-4">
+        
+        <div>
+            Ordnung Restklassenring
+        </div>
+        <div>
+            <select bind:value={rkrExponent}> 
+                    {#each primes as prime,i}
+                    <option value="{prime}">{prime}</option>
+                    {/each}
+            </select>
+        </div>
+        <div>hoch</div>
+        <input type="number" bind:value={primeBasis} min=1 max=100>
+        <div>
+           ergibt 𝔽<sub>{restKlassenRingOrdnung}</sub>
+        </div>
+    </div>
+</div>
+
+
+<style lang="postcss">
+    .flex{
+        margin-bottom: 20px;
+    }
+</style>
